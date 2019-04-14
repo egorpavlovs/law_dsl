bda58d21567793b5854838308013c37107251dcb
Egor Pavlov <pavlov_egor007@mail.ru>
2019-04-14 16:25:24 +0300

    Localize (ru) files methods

--- conditions/files_methods.rb
+++ conditions/files_methods.rb
@@ -1,20 +1,34 @@
 module FilesMethods
 
-  def фотофиксация_исходной_ситуации_представлена?(fotos)
+  def photos_initial_situation_presented?(fotos)
     allowed = !fotos.empty?
-    response = allowed ? allowed : "Фотофиксация исходной ситуации не представлена"
+    response = allowed ? I18n.t('photos_initial_situation_presented?(fotos).responses.fotos_present') : I18n.t('photos_initial_situation_presented?(fotos).responses.fotos_not_present')
     format_result_methods_response(allowed, response)
   end
 
-  def ситуационный_план_представлен?(plan)
+  def situational_plan_presented?(plan)
     allowed = !plan.nil?
-    response = allowed ? allowed : "Ситуационного плана не представлено"
+    response = allowed ? I18n.t('situational_plan_presented?(plan).responses.plan_present') : I18n.t('situational_plan_presented?(plan).responses.plan_not_present')
     format_result_methods_response(allowed, response)
   end
 
-  def изображения_проектируемых_конструкций_представлены?(fotos)
+  def photos_projected_constructions_presented?(fotos)
     allowed = !fotos.empty?
-    response = allowed ? allowed : "Не представлены изображения проектируемых конструкций"
+    response = allowed ? I18n.t('photos_projected_constructions_presented?(fotos).responses.fotos_present') : I18n.t('photos_projected_constructions_presented?(fotos).responses.fotos_not_present')
+    format_result_methods_response(allowed, response)
+  end
+
+  def топографический_план_с_маштабом_представлен?(plan, scale)
+    if plan['файл'].nil?
+      allowed = false
+      response = "Топографический план не представлен"
+    elsif plan['маштаб'] != scale
+      allowed = false
+      response = "Топографический план представлен в не подходящем формате"
+    else
+      allowed = true
+      response = "Топографический план представлен в подходящем формате"
+    end
     format_result_methods_response(allowed, response)
   end
 
