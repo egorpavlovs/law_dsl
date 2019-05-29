301844b71419d2eeeaffd01a8f05bbc75f8a9371
Egor Pavlov <pavlov_egor007@mail.ru>
2019-05-30 00:52:26 +0300

    Add new methods for egrul, fix applicant methods

--- conditions/applicant_methods.rb
+++ conditions/applicant_methods.rb
@@ -4,35 +4,45 @@ module ApplicantMethods
 
   def natural_person?(*args)
     method_name =__method__.to_s
-    allowed = args.first['role'] == "natural person"
+    key_with_data = args.first
+    key = key_with_data.first
+    allowed = key_with_data.last.dig(*%w(applicant role)) == "natural person"
     response = allowed ? "natural_person" : "not_natural_person"
     format_result_methods_response(method_name, allowed, response)
   end
 
   def legal_entity?(*args)
     method_name =__method__.to_s
-    allowed = args.first['role'] == "legal entity"
+    key_with_data = args.first
+    key = key_with_data.first
+    allowed = key_with_data.last.dig(*%w(applicant role)) == "legal entity"
     response = allowed ? "legal_entity" : "not_legal_entity"
     format_result_methods_response(method_name, allowed, response)
   end
 
   def individual_entrepreneur?(*args)
     method_name =__method__.to_s
-    allowed = args.first['role'] == "individual entrepreneur"
+    key_with_data = args.first
+    key = key_with_data.first
+    allowed = key_with_data.last.dig(*%w(applicant role)) == "individual entrepreneur"
     response = allowed ? "individual_entrepreneur" : "not_individual_entrepreneur"
     format_result_methods_response(method_name, allowed, response)
   end
 
   def agent?(*args)
     method_name =__method__.to_s
-    allowed = args.first['role'] == "agent"
+    key_with_data = args.first
+    key = key_with_data.first
+    allowed = key_with_data.last.dig(*%w(applicant role)) == "agent"
     response = allowed ? "agent" : "not_agent"
     format_result_methods_response(method_name, allowed, response)
   end
 
-def applicant_identity_esia?(*args)
+  def applicant_identity_esia?(*args)
+    key_with_data = args.first
+    key = key_with_data.first
     method_name =__method__.to_s
-    esia_data = args.first
+    esia_data = key_with_data.last[key]
     allowed = !esia_data.nil?
     response = allowed ? "esia_data_present" : "esia_data_not_present"
     format_result_methods_response(method_name, allowed, response)
--- conditions/egrul_methods.rb
+++ conditions/egrul_methods.rb
@@ -35,4 +35,52 @@ module EgrulMethods
     response = allowed ? 'applicant_can_represent_legal_entity' : 'applicant_cant_represent_legal_entity'
     format_result_methods_response(method_name, allowed, response)
   end
+
+  def region_registration_legal_entity_match_egrul?(*args)
+    method_name =__method__.to_s
+    egrul_region_data = args.first
+    key_name = egrul_region_data.first
+    egrul_region = egrul_region_data.last.dig(*%W(egrul_query_result СвЮЛ СвАдресЮЛ АдресРФ Регион #{key_name}))
+    input_region = args.last
+
+    allowed = input_region.downcase == egrul_region.downcase
+    response = allowed ? "region_registration_legal_entity_match" : "region_registration_legal_entity_not_match"
+    format_result_methods_response(method_name, allowed, response)
+  end
+
+  def city_registration_legal_entity_match_egrul?(*args)
+    method_name =__method__.to_s
+    egrul_city_data = args.first
+    key_name = egrul_city_data.first
+    egrul_city = egrul_city_data.last.dig(*%W(egrul_query_result СвЮЛ СвАдресЮЛ АдресРФ Город #{key_name}))
+    input_city = args.last
+
+    allowed = input_city.downcase == egrul_city.downcase
+    response = allowed ? "city_registration_legal_entity_match" : "city_registration_legal_entity_not_match"
+    format_result_methods_response(method_name, allowed, response)
+  end
+
+  def legal_entity_registered_more_given_years?(*args)
+    method_name =__method__.to_s
+    data = args.first
+    key_name = data.first
+    date_registration = data.last.dig(*%W(egrul_query_result СвЮЛ #{key_name}))
+    input_years = args.last
+
+    allowed = Time.new(date_registration).year <= Time.now.year - input_years.to_i
+    response = allowed ? "legal_entity_registered_more_given_years" : "legal_entity_registered_less_given_years"
+    format_result_methods_response(method_name, allowed, response)
+  end
+
+  def legal_entity_registered_in_given_pension_fund?(*args)
+    method_name =__method__.to_s
+    data = args.first
+    key_name = data.first
+    egrul_pension_fund_code = data.last.dig(*%W(egrul_query_result СвЮЛ СвРегПФ СвОргПФ #{key_name}))
+    input_code = args.last
+
+    allowed = input_code == egrul_pension_fund_code
+    response = allowed ? "legal_entity_registered_in_given_pension_fund" : "legal_entity_not_registered_in_given_pension_fund"
+    format_result_methods_response(method_name, allowed, response)
+  end
 end
